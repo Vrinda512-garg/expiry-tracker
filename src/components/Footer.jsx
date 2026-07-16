2558c68f5de135c1b56e9c2f94d6940544200297
Vrinda Garg <vrinda051206@gmail.com>
2026-07-16 20:13:03 +0530

    Created landing page components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer(){
+
+return(
+
+<footer>
+
+<p>
+© 2026 ExpiryAI. All rights reserved.
+</p>
+
+</footer>
+
+)
+
+}
+
+export default Footer;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+function Hero() {
+  return (
+    <section className="hero">
+
+      <h1>
+        Never Miss an Expiry Date Again
+      </h1>
+
+      <p>
+        Track your food, medicines and cosmetics
+        with smart expiry reminders.
+      </p>
+
+      <button>
+        Add Product
+      </button>
+
+    </section>
+  );
+}
+
+export default Hero;
